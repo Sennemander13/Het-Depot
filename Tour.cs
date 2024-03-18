dadac45739a9142314dd72b8eb84276b3c619978
LucasTjeerde <159786617+LucasWTJ@users.noreply.github.com>
2024-03-18 15:55:05 +0100

    Add BezoekerTour, change bezoeker.cs

--- Tour.cs
+++ Tour.cs
@@ -1,14 +1,15 @@
 public class Tour{
 
 
-
+    public int Id { get; set; }
     public string Start { get; set; }
     public string End { get; set; }
     public List<string> Spots;
     public List<string> HasTakenTour;
 
-    public Tour(string start,string end, List<string> spots, List<string> hastaken)
+    public Tour(int id, string start, string end, List<string> spots, List<string> hastaken)
     {
+        Id = id;
         Start = start;
         End = end;
         Spots = spots;
--- obj/Debug/net8.0/Het-Depot.AssemblyInfo.cs
+++ obj/Debug/net8.0/Het-Depot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Het-Depot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebb9bb90b8a6f314ec63a13958999ab5d77eefe9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bb3a9ecaf3cc1dda25739d15b681bfc80c51152")]
 [assembly: System.Reflection.AssemblyProductAttribute("Het-Depot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Het-Depot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
