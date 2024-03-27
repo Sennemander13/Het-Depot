a3ac65b8042cb9e3b8c378e7bc8e0753898c93b9
LucasTjeerde <159786617+LucasWTJ@users.noreply.github.com>
2024-03-27 09:46:25 +0100

    made conformation checks

--- BezoekerTour.cs
+++ BezoekerTour.cs
@@ -1,15 +1,35 @@
 static class BezoekerTour
 {
     public static Tour tour;
+    public static string uniqueCode;
 
     public static void display()
     {
         Console.WriteLine("-----------------------");
         Console.WriteLine($"{tour.Start} - {tour.End} is geselecteerd\nconformeer je keuze (ja of nee)");
+        string conformation = Console.ReadLine()!;
         Console.WriteLine("-----------------------");
+        AddID(uniqueCode, conformation);
     }
-    public static void AddID(int number)
+    public static void AddID(string uniqueCode, string conformation)
     {
-        
+        while (true)
+        {
+            if (conformation == "ja" || conformation == "Ja")
+            {
+                // nog unique code verwijderen uit UniqueCodesToday en tovoegen aan spots in de Tour.
+                Console.WriteLine($"tour: {tour.Start} - {tour.End} succesvol gereserveerd");
+                break;
+            }
+            else if (conformation == "nee" || conformation == "Nee")
+            {
+                Program.TryEnter(uniqueCode);
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Ongeldige invoer");
+            }
+        }
     }
 }
