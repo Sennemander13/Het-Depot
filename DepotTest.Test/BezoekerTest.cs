b67a65f6768e5ce1c33f940ee1f1e00376034f5d
LucasTjeerde <159786617+LucasWTJ@users.noreply.github.com>
2024-04-23 21:22:53 +0200

    making unit test bezoeker

--- DepotTest.Test/BezoekerTest.cs
+++ DepotTest.Test/BezoekerTest.cs
@@ -1,10 +1,51 @@
-namespace DepotTest.Test;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
 
-[TestClass]
-public class UnitTest1
+namespace BezoekerTests
 {
-    [TestMethod]
-    public void TestMethod1()
+    [TestClass]
+    public class BezoekerTests
     {
+        private StringWriter _consoleOutput;
+        private StringReader _consoleInput;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _consoleOutput = new StringWriter();
+            _consoleInput = new StringReader("A\n"); // Simulate user input of 'A' to log out
+            Console.SetOut(_consoleOutput);
+            Console.SetIn(_consoleInput);
+        }
+
+        [TestMethod]
+        public void Expected_when_pressed()
+        {
+            var expectedOutput = "-----------------------\r\n[A] log uit\r\nKies een rondleiding: ";
+
+            Bezoeker.main();
+
+            StringAssert.Contains(_consoleOutput.ToString(), expectedOutput);
+        }
+
+        [TestMethod]
+        public void Main_Displays_Tours()
+        {
+
+            Tours.tours = new System.Collections.Generic.List<Tour>
+            {
+                // make test tours
+                new Tour { Id = "1", Start = "Start1", End = "End1", Spots = new System.Collections.Generic.List<Spot>() },
+                new Tour { Id = "2", Start = "Start2", End = "End2", Spots = new System.Collections.Generic.List<Spot>() }
+            };
+            var expectedOutput = "-----------------------\r\n|1| Start1 - End1, 0/13\r\n|2| Start2 - End2, 0/13\r\n-----------------------\r\n[A] log uit\r\nKies een rondleiding: ";
+
+            Bezoeker.main();
+
+
+            StringAssert.Contains(_consoleOutput.ToString(), expectedOutput);
+        }
+
     }
 }
---
+++ DepotTest.Test/BezoekerstourTest.cs
@@ -0,0 +1,10 @@
+// namespace DepotTest.Test;
+
+// [TestClass]
+// public class UnitTest1
+// {
+//     [TestMethod]
+//     public void TestMethod1()
+//     {
+//     }
+// }
