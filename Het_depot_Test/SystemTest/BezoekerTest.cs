ba519e1ea753661879769966492d58cb2346ae42
LucasTjeerde <159786617+LucasWTJ@users.noreply.github.com>
2024-05-29 10:37:50 +0200

    add SystemTest

--- Het_depot_Test/SystemTest/BezoekerTest.cs
+++ Het_depot_Test/SystemTest/BezoekerTest.cs
@@ -1,36 +1,82 @@
-namespace Het_depot_Test;
-
-[TestClass]
-public class BezoekrsTest
+namespace Het_depot_Test
 {
-
-    [TestMethod]
-    [DataRow("1010,A,1,y,A,2,y,A,2,y,B,quit")]
-    public void TestBezoekerSystem(string inputString)
+    [TestClass]
+    public class BezoekersTest
     {
-        string[] splitArray = inputString.Split(',');
-        FakeWorld world = new()
+        [TestMethod]
+        [DataRow("1111,A,1,y,b,quit")]
+        public void TestTourBooking(string inputString)
         {
-            Now = new DateTime(2004, 08, 25),
-            LinesToRead = new List<string>(splitArray),
-            Files = new()
+            string[] splitArray = inputString.Split(',');
+            FakeWorld world = new()
             {
-                { "DataSources/UniqueCodesToday.json", "[\"1111\", \"1010\"]" },
-                { "DataSources/ListOfTours.json", "[{\"Id\": \"1\", \"Start\": \"11:40\", \"End\": \"12:20\", \"Spots\": [], \"HasTakenTour\": []}, {\"Id\": \"2\", \"Start\": \"12:40\", \"End\": \"13:20\", \"Spots\": [], \"HasTakenTour\": []}]"},
-                { "DataSources/GidsCodes.json", "[\"1\", \"2\"]"}
+                Now = new DateTime(2004, 08, 25),
+                LinesToRead = new List<string>(splitArray),
+                Files = new()
+                {
+                    { "DataSources/UniqueCodesToday.json", "[\"1111\", \"1010\"]" },
+                    { "DataSources/ListOfTours.json", "[{\"Id\": \"1\", \"Start\": \"11:40\", \"End\": \"12:20\", \"Spots\": [], \"HasTakenTour\": []}, {\"Id\": \"2\", \"Start\": \"12:40\", \"End\": \"13:20\", \"Spots\": [], \"HasTakenTour\": []}]" },
+                    { "DataSources/GidsCodes.json", "[\"1\", \"2\"]" }
+                }
+            };
+            Program.world = world;
+            Program.Main();
+            foreach (string l in world.LinesWritten)
+            {
+                Console.WriteLine(l);
             }
-        };
-        Program.world = world;
-
-        Program.Main();
-
-        foreach (string l in world.LinesWritten)
-        {
-            Console.WriteLine(l);
+            Assert.AreEqual(true, world.LinesWritten.Contains("Wilt u op deze rondleiding een plaats reserveren? (y/n)"));
         }
-        Assert.AreEqual(true, world.LinesWritten.Contains("Wilt u op deze rondleiding een plaats reserveren? (y/n)"));
-        Assert.AreEqual(true, world.LinesWritten.Contains("Wilt u herboeken naar deze rondleiding? (y/n)"));
-        Assert.AreEqual(true, world.LinesWritten.Contains("U heeft al gereserveerd op deze rondleiding"));
 
+        [TestMethod]
+        [DataRow("1111,A,2,y,A,1,y,b,quit")]
+        public void TestRebooking(string inputString)
+        {
+            string[] splitArray = inputString.Split(',');
+            FakeWorld world = new()
+            {
+                Now = new DateTime(2004, 08, 25),
+                LinesToRead = new List<string>(splitArray),
+                Files = new()
+                {
+                    { "DataSources/UniqueCodesToday.json", "[\"1111\", \"1010\"]" },
+                    { "DataSources/ListOfTours.json", "[{\"Id\": \"1\", \"Start\": \"11:40\", \"End\": \"12:20\", \"Spots\": [], \"HasTakenTour\": []}, {\"Id\": \"2\", \"Start\": \"12:40\", \"End\": \"13:20\", \"Spots\": [], \"HasTakenTour\": []}]" },
+                    { "DataSources/GidsCodes.json", "[\"1\", \"2\"]" }
+                }
+            };
+            Program.world = world;
+            Program.Main();
+            foreach (string l in world.LinesWritten)
+            {
+                Console.WriteLine(l);
+            }
+            Assert.AreEqual(true, world.LinesWritten.Contains("Wilt u herboeken naar deze rondleiding? (y/n)"));
+        }
+
+        [TestMethod]
+        [DataRow("1111,A,1,y,A,2,y,A,2,y,B,quit")]
+        public void TestAlreadyReserved(string inputString)
+        {
+            string[] splitArray = inputString.Split(',');
+            FakeWorld world = new()
+            {
+                Now = new DateTime(2004, 08, 25),
+                LinesToRead = new List<string>(splitArray),
+                Files = new()
+                {
+                    { "DataSources/UniqueCodesToday.json", "[\"1111\", \"1010\"]" },
+                    { "DataSources/ListOfTours.json", "[{\"Id\": \"1\", \"Start\": \"11:40\", \"End\": \"12:20\", \"Spots\": [], \"HasTakenTour\": []}, {\"Id\": \"2\", \"Start\": \"12:40\", \"End\": \"13:20\", \"Spots\": [], \"HasTakenTour\": []}]" },
+                    { "DataSources/GidsCodes.json", "[\"1\", \"2\"]" }
+                }
+            };
+            Program.world = world;
+            Program.Main();
+            foreach (string l in world.LinesWritten)
+            {
+                Console.WriteLine(l);
+            }
+            Assert.AreEqual(true, world.LinesWritten.Contains("U heeft al gereserveerd op deze rondleiding"));
+        }
     }
 }
+
