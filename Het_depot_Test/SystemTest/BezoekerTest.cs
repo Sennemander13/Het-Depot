3669f149a4565874961118bdd1f8aa926244dc1b
LucasTjeerde <159786617+LucasWTJ@users.noreply.github.com>
2024-06-14 19:49:49 +0200

    Change system test bezoeker

--- Het_depot_Test/SystemTest/BezoekerTest.cs
+++ Het_depot_Test/SystemTest/BezoekerTest.cs
@@ -1,89 +1,84 @@
-namespace Het_depot_Test
+
+[TestClass]
+public class BezoekersTest
 {
-    [TestClass]
-    public class BezoekersTest
+    [TestMethod]
+    public void TestTourBooking()
     {
-        [TestMethod]
-        [DataRow("1111,A,1,y,b,quit")]
-        public void TestTourBooking(string inputString)
+        FakeWorld world = new()
         {
-            string[] splitArray = inputString.Split(',');
-            FakeWorld world = new()
-            {
-                Now = new DateTime(2004, 08, 25),
-                LinesToRead = new List<string>(splitArray),
-                Files = new()
-                {
-                    { "DataSources/UniqueCodesToday.json", "[\"1111\", \"1010\"]" },
-                    { "DataSources/ListOfTours.json", "[{\"Id\": \"1\", \"Start\": \"11:40\", \"End\": \"12:20\", \"Spots\": [], \"HasTakenTour\": []}, {\"Id\": \"2\", \"Start\": \"12:40\", \"End\": \"13:20\", \"Spots\": [], \"HasTakenTour\": []}]" },
-                    { "DataSources/GidsCodes.json", "[\"1\", \"2\"]" },
-                    { "RondleidingLog/default.json", "[{\"Id\": \"1\", \"Start\": \"11:40\", \"End\": \"12:20\", \"Spots\": [], \"HasTakenTour\": []}, {\"Id\": \"2\", \"Start\": \"12:40\", \"End\": \"13:20\", \"Spots\": [], \"HasTakenTour\": []}]"},
-                    // { }
-
-                }
-            };
-            Program.world = world;
-            Program.Main();
-            foreach (string l in world.LinesWritten)
-            {
-                Console.WriteLine(l);
-            }
-            Assert.AreEqual(true, world.LinesWritten.Contains("Wilt u op deze rondleiding een plaats reserveren? (y/n)"));
+            LinesToRead = new List<string>() { "1111", "A", "1", "Y", "", "B", "quit" }
+        };
+        Program.world = world;
+        Program.Main();
+        foreach (string l in world.LinesWritten)
+        {
+            Console.WriteLine(l);
         }
 
-        [TestMethod]
-        [DataRow("1111,A,2,y,b,quit")]
-        public void TestRebooking(string inputString)
-        {
-            string[] splitArray = inputString.Split(',');
-            FakeWorld world = new()
-            {
-                Now = new DateTime(2004, 08, 25),
-                LinesToRead = new List<string>(splitArray),
-                Files = new()
-                {
-                    { "DataSources/UniqueCodesToday.json", "[\"1111\", \"1010\"]" },
-                    { "RondleidingLog/default.json", "[{\"Id\": \"1\", \"Start\": \"11:40\", \"End\": \"12:20\", \"Spots\": [], \"HasTakenTour\": []}, {\"Id\": \"2\", \"Start\": \"12:40\", \"End\": \"13:20\", \"Spots\": [], \"HasTakenTour\": []}]" },
-                    { "DataSources/GidsCodes.json", "[\"1\", \"2\"]" }
-                    // { "RondleidingLog/default.json", "[{\"Id\": \"1\", \"Start\": \"11:40\", \"End\": \"12:20\", \"Spots\": [], \"HasTakenTour\": []}]"},
+        Assert.AreEqual(true, BezoekerTour.tour == DataModel.listoftours[1]);
+        Assert.AreEqual(true, world.LinesWritten.Contains("Kies een Rondleiding (id): "));
+        Assert.AreEqual(true, world.LinesWritten.Contains("Wilt u op deze rondleiding een plaats reserveren?"));
+        Assert.AreEqual(true, world.LinesWritten.Contains("[Y]: Reserveren"));
+        Assert.AreEqual(true, world.LinesWritten.Contains("Reserveren voltooid!"));
+        Assert.AreEqual(true, world.LinesWritten.Contains("Druk Enter"));
+        Assert.AreEqual(true, world.ReadAllText("RondleidingLog/14-06-2024.json").Contains(@"{""Id"":""1"",""Start"":""11:30"",""Spots"":[""1111""],""HasTakenTour"":[],""GuideCode"":""""}"));
 
-                }
-            };
-            Program.world = world;
-            Program.Main();
-            foreach (string l in world.LinesWritten)
-            {
-                Console.WriteLine(l);
-            }
-            Assert.AreEqual(true, world.LinesWritten.Contains("Wilt u herboeken naar deze rondleiding? (y/n)"));
+        Assert.IsTrue(world.LinesWritten.Contains("|1|11:30, 12 plekken vrij"));
+    }
+
+
+    [TestMethod]
+    public void TestRebooking()
+    {
+        FakeWorld world = new()
+        {
+            LinesToRead = new List<string>() { "1111", "A", "2", "Y", "", "B", "quit" }
+        };
+        Program.world = world;
+        Program.Main();
+        foreach (string l in world.LinesWritten)
+        {
+            Console.WriteLine(l);
         }
 
-        [TestMethod]
-        [DataRow("1111,A,1,y,A,2,y,A,2,y,B,quit")]
-        public void TestAlreadyReserved(string inputString)
-        {
-            string[] splitArray = inputString.Split(',');
-            FakeWorld world = new()
-            {
-                Now = new DateTime(2004, 08, 25),
-                LinesToRead = new List<string>(splitArray),
-                Files = new()
-                    {
-                        { "DataSources/UniqueCodesToday.json", "[\"1111\", \"1010\"]" },
-                        { "DataSources/ListOfTours.json", "[{\"Id\": \"1\", \"Start\": \"11:40\", \"End\": \"12:20\", \"Spots\": [], \"HasTakenTour\": []}, {\"Id\": \"2\", \"Start\": \"12:40\", \"End\": \"13:20\", \"Spots\": [], \"HasTakenTour\": []}]" },
-                        { "DataSources/GidsCodes.json", "[\"1\", \"2\"]" },
-                        { "RondleidingLog/default.json", "[{\"Id\": \"1\", \"Start\": \"11:40\", \"End\": \"12:20\", \"Spots\": [], \"HasTakenTour\": []}]"},
+        Assert.AreEqual(true, BezoekerTour.tour == DataModel.listoftours[2]);
+        Assert.AreEqual(true, world.LinesWritten.Contains("Kies een Rondleiding (id): "));
+        Assert.AreEqual(true, world.LinesWritten.Contains("U heeft al gereserveerd op de rondleiding van 11:30"));
+        Assert.AreEqual(true, world.LinesWritten.Contains("Wilt u herboeken naar deze rondleiding?"));
+        Assert.AreEqual(true, world.LinesWritten.Contains("[Y]: Herboeken"));
+        Assert.AreEqual(true, world.LinesWritten.Contains("Herboeken voltooid!"));
+        Assert.AreEqual(true, world.LinesWritten.Contains("Druk Enter"));
 
-                    }
-            };
-            Program.world = world;
-            Program.Main();
-            foreach (string l in world.LinesWritten)
-            {
-                Console.WriteLine(l);
-            }
-            Assert.AreEqual(true, world.LinesWritten.Contains("U heeft al gereserveerd op deze rondleiding"));
+        Assert.AreEqual(false, world.ReadAllText("RondleidingLog/14-06-2024.json").Contains(@"{""Id"":""1"",""Start"":""11:30"",""Spots"":[""1111""],""HasTakenTour"":[],""GuideCode"":""""}"));
+        Assert.AreEqual(true, world.ReadAllText("RondleidingLog/14-06-2024.json").Contains(@"{""Id"":""2"",""Start"":""11:50"",""Spots"":[""1111""],""HasTakenTour"":[],""GuideCode"":""""}"));
+
+        Assert.IsTrue(world.LinesWritten.Contains("|2|11:50, 12 plekken vrij"));
+
+    }
+
+
+    [TestMethod]
+    public void TestAlreadyReserved()
+    {
+        FakeWorld world = new()
+        {
+            LinesToRead = new List<string>() { "1111", "A", "2", "Y", "", "B", "quit" }
+        };
+        Program.world = world;
+        Program.Main();
+        foreach (string l in world.LinesWritten)
+        {
+            Console.WriteLine(l);
         }
+        Assert.AreEqual(true, BezoekerTour.tour == DataModel.listoftours[2]);
+        Assert.AreEqual(true, world.LinesWritten.Contains("Kies een Rondleiding (id): "));
+        Assert.AreEqual(true, world.LinesWritten.Contains("[Y]: Uitschrijven"));
+        Assert.AreEqual(true, world.LinesWritten.Contains("Uitschrijven voltooid!"));
+        Assert.AreEqual(true, world.LinesWritten.Contains("Druk Enter"));
+        Assert.AreEqual(false, world.ReadAllText("RondleidingLog/14-06-2024.json").Contains(@"{""Id"":""2"",""Start"":""11:50"",""Spots"":[""1111""],""HasTakenTour"":[],""GuideCode"":""""}"));
+        Assert.IsTrue(world.LinesWritten.Contains("|2|11:50, 13 plekken vrij"));
+        Assert.IsTrue(false);
+
     }
 }
-
